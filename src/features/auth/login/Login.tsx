2c96b85ea20e5e5227b08fbc2508b811563c24e5
jrussumbrella <jrussclay@gmail.com>
2021-06-16 17:04:31 +0800

    added logo to login and register page

--- src/features/auth/login/Login.tsx
+++ src/features/auth/login/Login.tsx
@@ -1,12 +1,10 @@
 import { useState } from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import { useForm, Controller } from 'react-hook-form';
@@ -54,6 +52,10 @@ const useStyles = makeStyles((theme) => ({
   alertContainer: {
     marginTop: 20,
     width: '100%',
+  },
+  logo: {
+    width: 60,
+    marginBottom: 10,
   },
 }));
 
@@ -118,9 +120,11 @@ const Login = () => {
       />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
+          <img
+            src='/images/logo.svg'
+            alt='budgetty logo'
+            className={classes.logo}
+          />
           <Typography component='h1' variant='h5'>
             Log in
           </Typography>

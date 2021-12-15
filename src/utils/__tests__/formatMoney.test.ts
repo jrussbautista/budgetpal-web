46b8a766c5b980dcd7ed6b2456ebc62098f9f64c
jrussumbrella <jrussclay@gmail.com>
2021-12-15 20:34:30 +0800

    Add unit test for formatMoney util

---
+++ src/utils/__tests__/formatMoney.test.ts
@@ -0,0 +1,11 @@
+import formatMoney from '@/utils/formatMoney';
+
+test('correctly format price with default currency', () => {
+  expect(formatMoney(500)).toBe('$500.00');
+  expect(formatMoney(4000)).toBe('$4,000.00');
+});
+
+test('correctly format price with different currency', () => {
+  expect(formatMoney(600, 'PHP')).toBe('₱600.00');
+  expect(formatMoney(10000, 'PHP')).toBe('₱10,000.00');
+});
